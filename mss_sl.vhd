af796d538f68de8803f07997e6ca5b734028256b
Antonella-garcia <74313741+Antonella-garcia@users.noreply.github.com>
2021-04-23 19:09:24 -0500

    Update mss_sl.vhd

--- mss_sl.vhd
+++ mss_sl.vhd
@@ -9,8 +9,8 @@ use ieee.std_logic_1164.all;
 entity mss_sl is
 	port(
 		resetn,clk,start,cero: in std_logic;
-		est: out std_logic_vector(1 downto 0);
-		enbcd,endown,fin: out std_logic);
+		est: out std_logic_vector(2 downto 0);
+		enbcd,endown,resetp,fin: out std_logic);
 end mss_sl;
 
 --Architecture
@@ -19,7 +19,7 @@ architecture solve of mss_sl is
 	--ta,tb,tc,td
 	--start
 	
-	type estado is (ta,tb,tc,td);
+	type estado is (ta,tb,tc,td,te,tf);
 	signal y: estado;
 	begin
 	--Process #1: Next state decoder and state memory
@@ -38,7 +38,13 @@ architecture solve of mss_sl is
 					when tc => 
 							  if cero='1' then y <= td;
 							  else y <= tc; end if;
-					when td =>y <= ta;
+					when td =>y <= te;
+					when te =>
+								if start='0' then y <= te;
+							  else y <= tf; end if;	
+					when tf =>
+								if start='1' then y <= tf;
+							  else y <= ta; end if;							  
 				end case;
 			end if;
 	end process;
@@ -48,15 +54,17 @@ architecture solve of mss_sl is
 		begin
 			enbcd<='0';
 			endown<='0';
-			fin<='0';
-			est<="00";
+			est<="000";
 			case y is
-				when ta => est<="00"; --start 0
-				when tb => est<="01"; --start 1
-				when tc => enbcd<='1';endown <='1'; est<="10";
-				when td => fin<='1'; est<="11"; 
-				
-				
+				when ta => est<="000"; --start 0
+				when tb => est<="001"; --start 1
+				when tc => enbcd<='1';endown <='1'; est<="010";
+				when td => est<="011"; 
+				when te => est<="100"; 
+				when tf => est<="101"; 
+			
 			end case;
 	end process;
+			fin<= '1' when (y=te) or (y=tf) else '0';
+			resetp<= '0' when y=ta else '1';
 end solve;
